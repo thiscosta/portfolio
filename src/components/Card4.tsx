d0f1a8d4a0f01ebe8c18f9a8adb7ef147a8ae158
Thiago Costa <37018497+thiscosta@users.noreply.github.com>
2022-08-17 16:38:43 -0300

    Delete Card4.tsx

--- src/components/Card4.tsx
+++
@@ -1,5 +0,0 @@
-ü*&¦‰ÞžØêÚµç[Éž²Õ€DÒžv+bžaŠÇ(šš'z{[Ê§w*.Â)e–‹¶šn)b·+h­èêÚµëayÊ{­†‹­r‰ß–'-±:)­ë½ëajÖ›Š[rj\ÊÇ±¦Ší~º&Bç¬¶Ú+z§z¶­zØb±ø¥y:,zÛ©¦º)±¸§v)à±©ÝiËb¢{.±ëayž²×ŠÚ+
-‡^.ˆ"r
-²)í…èh¢F¬²‡"j×Â+a¶¬r‰©¢w§´Z+…éijw_º»az·^µ¨¥²·Ÿz»h†Ûi³ÿðÃ
-®zËZ‹÷hrÏâšš+¶Ër•ç_®‰¦º/æj×«‰©²Ü¥zÈ¦¦Ší	ªÝàúè¦Çë¢oíÊ—¬Šjh®Û¬x&«w‡ë¢oî±àš­Þ¢{-O*^Bç¬µ©ò²Ü¥y×b¾Ø^™æ§É¶œ’
-èºwB¢Z+®ÚÛžvç¹çVèÅ(ZvŒ-…éžrë-¢d¡iÚ0°I^½«b¢~›£¡iÚ0nŠÝz´Zv+¬òœ]ŠÊek'å{h²+b¢zÞ•«b½è¬¢V­Š‰â²‰Zµçå{â­ç-Š‰Ü¢[¦Ÿ¶ýÒœcºËb ¨ž×§µù^ÆËZ®Ö¥Š	Èµé¬~W±²Ö«¶–v)àÒœ[£¢Î)ànŠÝz¶èÆ‹Þ­ùhÂuéÜ¢{-"f xZÚ™æ§ÊËr•ç]Š÷b²™ZÉù^Æš,ŠØ¨ž·¥jØ¯z+(•«b¢x¬¢V­yù^Ä8«yËb¢w(–é§Žë-‰ü‚¢{^ž×å{-j»Z–('"×¦±ù^ÆËZ®ÚZuØ§ƒ])Åº1J,âžè­×«nŒZ–('Ié_²ÚÞµÈfj¸"ŸJqr‰ì´‰šæ§ÊËr•ç]Šû!¢é]Šðj·O®Ši®Š
